14a8e668adf812d35ef74f42784d7bbbd5c801ae
Gio <me@giobautista.com>
2019-03-22 21:16:58 -0400

    Create diskAlert.sh

---
+++ diskAlert.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  used=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $used -ge 90 ]; then
+    curl -X POST https://textbelt.com/text --data-urlencode phone='+15551234567' --data-urlencode message="$(date): The partition \"$partition\" on $(hostname) is at $used%" -d key=YOURKEY
+  fi
+done
